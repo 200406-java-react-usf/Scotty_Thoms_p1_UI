c1d9e0fe6630bd29c57357d9d2c58555c6eeeb2d
Scotty Thoms <spthoms19@gmail.com>
2020-05-14 15:49:10 -0500

    trying agian to work with s3 bucket

--- src/remote/auth-service.ts
+++ src/remote/auth-service.ts
@@ -5,12 +5,3 @@ export async function authenticate(username: string, password: string) {
     return await response.data;
 }
 
-export async function register(username: string, password: string, firstName: string, lastName: string, email: string) {
-    let response = await reimbursmentClient.post('/users', {username, password, firstName, lastName, email});
-    return await response.data;
-} 
-
-export async function getAllUsers() {
-    let response = await reimbursmentClient.get('/users');
-    return await response.data;
-}
---
+++ src/remote/user-service.ts
@@ -0,0 +1,11 @@
+import { reimbursmentClient } from './reimbursment-client';
+
+export async function register(username: string, password: string, firstName: string, lastName: string, email: string) {
+    let response = await reimbursmentClient.post('/users', {username, password, firstName, lastName, email});
+    return await response.data;
+} 
+
+export async function getAllUsers() {
+    let response = await reimbursmentClient.get('/users');
+    return await response.data;
+}
