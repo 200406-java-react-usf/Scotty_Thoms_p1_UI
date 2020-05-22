c2ef5db00f41278214e5a082032d3f1dafd53819
Scotty Thoms <spthoms19@gmail.com>
2020-05-21 23:24:34 -0500

    changed baseURL for master merge

--- src/remote/reimbursement-client.ts
+++ src/remote/reimbursement-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const reimbursmentClient = axios.create({
-    // baseURL: 'http://scottythomsp1-env.eba-s3wy3xp3.us-east-2.elasticbeanstalk.com/',
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://scottythomsp1-env.eba-s3wy3xp3.us-east-2.elasticbeanstalk.com/',
+    // baseURL: 'http://localhost:8080/',
     headers: {
         'Content-Type': 'application/json'
     },
